b7798410ea8d12bbe2e7d9ccde524c9cfd9c1c2e
Hesi Taka Maulana <hesimaulana@gmail.com>
2022-03-17 09:35:11 +0700

    mengerjakan two-dimention-area-cp

--- backend/basic-golang/exercise/two-dimention-area-cp/main.go
+++ backend/basic-golang/exercise/two-dimention-area-cp/main.go
@@ -35,5 +35,34 @@ import (
 // - (4) Luas Lingkaran adalah : 50.240000
 
 func main() {
-	// TODO: answer here
+	var num int
+	var sisi float64 = 0
+	var sisi2 float64 = 0
+	fmt.Println("Input:")
+	fmt.Print("1: Rectange Area \n2: Rectangular Area \n3: Triangle Area \n4: Circle Area \n")
+	fmt.Println("Enter choice 1, 2, 3, or 4: 1 | 2 | 3 | 4")
+	fmt.Scanln(&num)
+	switch num {
+	case 1:
+		fmt.Println("Masukkan sisi")
+		fmt.Scan(&sisi)
+		var area = sisi * sisi
+		fmt.Print(area)
+	case 2:
+		fmt.Println("Masukkan alas dan panjang")
+		fmt.Scan(&sisi)
+		fmt.Scan(&sisi2)
+		var area = sisi * sisi2
+		fmt.Print(area)
+	case 3:
+		fmt.Println("Masukkan alas dan tinggi")
+		fmt.Scan(&sisi)
+		fmt.Scan(&sisi2)
+		var area = (sisi * sisi2) / 2
+		fmt.Print(area)
+	case 4:
+		fmt.Println("Masukkan jari jari")
+		fmt.Scan(&sisi)
+		fmt.Print(math.Pi * sisi * sisi)
+	}
 }
